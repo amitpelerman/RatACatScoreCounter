d9f8e6924a25e89f22561d4fed8e13132ec5810e
amitpelerman <37559920+amitpelerman@users.noreply.github.com>
2019-02-26 20:01:07 +0200

    Update and rename styleLabels.java to StyleLabels.java

--- app1/StyleLabels.java
+++ app1/StyleLabels.java
@@ -7,4 +7,4 @@ public interface styleLabels {
 	public String buttonStyle = "-fx-font-size: 38px;-fx-text-fill: rgb(110,0,220)";
 	public String namesStyle = "-fx-font-size: 20px;-fx-text-fill: rgb(110,0,220)";
 	public String warningStyle = "-fx-font-size: 35px;-fx-text-fill: rgb(255,40,0)";
-}
+}S
